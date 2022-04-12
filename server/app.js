211a0f463da51af0ae6be33012e3e1198e1cfbb8
Fatma Atasoy <76038509+Fatmats@users.noreply.github.com>
2022-04-12 15:25:00 +0300

    init

--- server/app.js
+++ server/app.js
@@ -32,6 +32,6 @@ mongoose.connect(process.env.DATABASE, {
     }
 });
 
-app.listen(3000, function(){
-    console.log("port listen ...")
-})
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
